7f208f463cb49277290e346419bec03e1bd1bedf
Cooper Yang <misairu.gugu@gmail.com>
2016-06-05 15:29:27 +0800

    lab-1.py fix complete

--- lab-1.py
+++ lab-1.py
@@ -3,7 +3,7 @@
 OS Course Exp - 1: consumer and producer
 
 Usage:
-	python lab-1.py [number of producer] [number of consumer]
+	python lab-1.py [number of producer] [number of consumer] [number of message]
 
 1. 场景1
   * 同一进程内启动一组生产者线程和一组消费者线程
@@ -12,27 +12,30 @@ Usage:
   * 启动一组生产者进程和一组消费者进程
   * 同一个数据文件为缓冲区
 """
-from threading import Thread
-from multiprocessing import Process, Queue, Semaphore, Lock, Manager
+import threading
+import multiprocessing
+from multiprocessing import Queue
 from codecs import encode, decode
 from time import sleep
 from random import randint
 import sys
 
 # init
-# Queue object contain a semaphore mechanism itself
+# Length of Queue object
 LENGTH = 4
-SHARE_ZONE = None
-FULL = None
-EMPTY = None
-MUTEX = None
-STATUS = None
+# number of message you want to display
+NUM_OF_MESSAGE = 20
 
 DOC = __doc__
 
-def producer(num=None, mode=None, output=None):
+def producer(share_zone=None, full=None, empty=None, mutex=None, status=None, num=None, mode=None, output=None):
 	"""
 	producer
+	:type share_zone: Queue
+	:type full: Semaphore
+	:type empty: Semaphore
+	:type mutex: Lock
+	:type status: Lock
 	:type num: int
 	:type mode: int
 	:type output: Queue
@@ -41,17 +44,17 @@ def producer(num=None, mode=None, output=None):
 		# roll the dice, if it is 0, do nothing at this cycle
 		roll = randint(0, 1)
 		if roll == 1:
-			with STATUS:
-				when_enter = SHARE_ZONE.qsize()
+			with status:
+				when_enter = share_zone.qsize()
 				if when_enter == LENGTH:
 					if mode == 1:
 						print 'producer ' + str(num) + ': is waiting' + '\n'
 					else:
 						output.put('producer ' + str(num) + ': is waiting' + '\n')
-			FULL.acquire()
-			with MUTEX:
-				temp = SHARE_ZONE.qsize()
-				SHARE_ZONE.put_nowait('data')
+			full.acquire()
+			with mutex:
+				temp = share_zone.qsize()
+				share_zone.put('data')
 				if mode == 1:
 					if when_enter == LENGTH:
 						print 'producer ' + str(num) + ': ' + str(temp) + ' -> ' + str(temp+1) + ' resume' + '\n'
@@ -62,15 +65,19 @@ def producer(num=None, mode=None, output=None):
 						output.put('producer ' + str(num) + ': ' + str(temp) + ' -> ' + str(temp + 1) + ' resume' + '\n', block = True, timeout = 5)
 					else:
 						output.put('producer ' + str(num) + ': ' + str(temp) + ' -> ' + str(temp + 1) + '\n', block = True, timeout = 5)
-			EMPTY.release()
+			empty.release()
 		else:
 			pass
 		sleep(0.5)
 	return
 
-def consumer(num=None, mode=None, output=None, ):
+def consumer(share_zone=None, full=None, empty=None, mutex=None, status=None, num=None, mode=None, output=None):
 	"""
 	consumer
+	:type full: Semaphore
+	:type empty: Semaphore
+	:type mutex: Lock
+	:type status: Lock
 	:type num: int
 	:type mode: int
 	:type output: Queue
@@ -79,17 +86,17 @@ def consumer(num=None, mode=None, output=None, ):
 		#roll the dice, if it is 0, do nothing at this cycle
 		roll = randint(0, 1)
 		if roll == 1:
-			with STATUS:
-				when_enter = SHARE_ZONE.qsize()
+			with status:
+				when_enter = share_zone.qsize()
 				if when_enter == 0:
 					if mode == 1:
 						print 'consumer ' + str(num) + ': is waiting' + '\n'
 					else:
 						output.put('consumer ' + str(num) + ': is waiting' + '\n')
-			EMPTY.acquire()
-			with MUTEX:
-				temp = SHARE_ZONE.qsize()
-				SHARE_ZONE.get_nowait()
+			empty.acquire()
+			with mutex:
+				temp = share_zone.qsize()
+				share_zone.get()
 				if mode == 1:
 					if when_enter == 0:
 						print 'consumer ' + str(num) + ': ' + str(temp) + ' -> ' + str(temp-1) + ' resume' + '\n'
@@ -100,7 +107,7 @@ def consumer(num=None, mode=None, output=None, ):
 						output.put( 'consumer ' + str(num) + ': ' + str(temp) + ' -> ' + str(temp - 1) + ' resume' + '\n', block = True, timeout = 5)
 					else:
 						output.put( 'consumer ' + str(num) + ': ' + str(temp) + ' -> ' + str(temp - 1) + '\n', block = True, timeout = 5)
-			FULL.release()
+			full.release()
 		else:
 			pass
 		sleep(0.5)
@@ -115,6 +122,10 @@ def main_func(input_argv=None):
 		producer_count = int(input_argv[1])
 		consumer_count = int(input_argv[2])
 		mode = int(input_argv[3])
+		try:
+			message_num = int(input_argv[4])
+		except IndexError:
+			message_num = NUM_OF_MESSAGE
 		if mode == 1 or mode == 2:
 			pass
 		else:
@@ -123,42 +134,49 @@ def main_func(input_argv=None):
 		print encode(decode(DOC, 'utf-8'), 'gbk')
 		raise NameError
 	if mode == 1:
+		share_zone = Queue(LENGTH)
+		full = threading.Semaphore(LENGTH)
+		empty = threading.Semaphore(0)
+		mutex = threading.Lock()
+		status = threading.Lock()
 		for i in range(0, producer_count):
-			tmp_thread = Thread(target = producer, args=(i+1, mode, ))
+			tmp_thread = threading.Thread(target=producer, args=(share_zone, full, empty, mutex, status, i+1, mode, ))
 			tmp_thread.daemon = True
 			tmp_thread.start()
 		for i in range(0, consumer_count):
-			tmp_thread = Thread(target = consumer, args=(i+1, mode, ))
+			tmp_thread = threading.Thread(target=consumer, args=(share_zone, full, empty, mutex, status, i+1, mode, ))
 			tmp_thread.daemon = True
 			tmp_thread.start()
 		while True:
 			pass
 	if mode == 2:
+		share_zone = multiprocessing.Queue(LENGTH)
+		full = multiprocessing.Semaphore(LENGTH)
+		empty = multiprocessing.Semaphore(0)
+		mutex = multiprocessing.Lock()
+		status = multiprocessing.Lock()
+		# open output file
 		output_file = open('lab-1.result', 'w')
-		output = Queue(50)
+		output = multiprocessing.Queue(50)
 		process_list = list()
 		for i in range(0, producer_count):
-			tmp_process = Process(target = producer, args = (i + 1, mode, output, ))
+			tmp_process = multiprocessing.Process(target=producer, args=(share_zone, full, empty, mutex, status, i+1, mode, output, ))
 			tmp_process.daemon = True
 			tmp_process.start()
-			process_list.append(tmp_process.pid)
+			process_list.append(tmp_process)
 		for i in range(0, consumer_count):
-			tmp_process = Process(target = consumer, args = (i + 1, mode, output, ))
+			tmp_process = multiprocessing.Process(target=consumer, args=(share_zone, full, empty, mutex, status, i+1, mode, output, ))
 			tmp_process.daemon = True
 			tmp_process.start()
-			process_list.append(tmp_process.pid)
-		while True:
-			string = output.get(block = True, timeout = 5)
+			process_list.append(tmp_process)
+		count = 0
+		while count < message_num:
+			string = output.get(block = True)
 			print string
 			output_file.write(string)
-	return
+			count += 1
+		output_file.close()
 
 if __name__ == "__main__":
 #调用主函数
-	manager = Manager()
-	SHARE_ZONE = Queue(LENGTH)
-	FULL = manager.Semaphore(LENGTH)
-	EMPTY = manager.Semaphore(0)
-	MUTEX = manager.Lock()
-	STATUS = manager.Lock()
 	main_func(sys.argv)
